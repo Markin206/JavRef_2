9c87d07a656bd7f67ab4da7388c547bc0f7a2297
Markin206 <marci061123@gmail.com>
2025-04-14 18:40:16 +0200

    form letrehozasa sima

--- Forradalmak/main.js
+++ Forradalmak/main.js
@@ -32,5 +32,37 @@ tableSim.appendChild(tbody);//hozzáadjuk a táblázathoz a táblázat tőrzsét
 
 const formDiv = makeDiv('form');//létrehozunk egy divet amely a form osztályt tartalmazza a makeDiv függvény által
 
+const formSim = document.createElement('form');//létrehozzuk a form elementet
+formDiv.appendChild(formSim)//a formot hozzáadjuk a formDivhez
+const fieldElementList = [{//létrehozunk egy tömböt amely tartalmaz 2 tulajdonságot fieldid amely tartalmazza a field Id-jét és fieldLabel amely tartalmazza a szöveget
+    fieldid: 'name',//a fieldid tulajdonság megkapja a name string értéket
+    fieldLabel: 'név'//a fieldLabel megkapja a név string értéket
+},
+{
+    fieldid: 'birth',//a fieldid tulajdonság megkapja a birth string értéket
+    fieldLabel: 'születési év'// a field label tulajdonság megkapja a születési év string értéket
+},
+{
+    fieldid: 'zipcode',//a fieldid tulajdonság megkapja a zipcode string értéket
+    fieldLabel: 'irányítószám'// a field label tulajdonság megkapja a irányítószám string értéket
+}]
+
+for(const fieldElement of fieldElementList){//egy for ciklussal végig iterláunk a fieldElementList tömbön és benne létrehozzuk a fieldeket annak label-jét, input-ját és tulajdonságait
+    const field = makeDiv('field');//létrehozzuk a field osztályú divet a makeDiv függvénnyel
+    formSim.appendChild(field);//hozzáadjuk a field osztályú divet a formhoz
+    const label = document.createElement('label');//létrehozzuk a field label elementjét
+    label.htmlFor = fieldElement.fieldid;//a labelnek megadunk egy htmlFor tulajdonság ami az elem fieldid-ja lesz
+    label.textContent = fieldElement.fieldLabel;//a labelnek megadunk egy textContent tulajdonság ami az elem fieldLabelje lesz
+    field.appendChild(label)// a labelt hozzáadjuk a field osztályú divhez
+    const input = document.createElement('input');//létrehozzuk a field input elementjét
+    input.id = fieldElement.fieldid;//az input id-ját megadjuk az elem fieldid tulajdonságával
+    field.appendChild(document.createElement('br'))//a divhez hozzáadunk a benne létrehozott sortörést
+    field.appendChild(input)//az inputot hozzáadjuk a divhez
+}
+
+const buttonFormSim = document.createElement('button');//létrehozzuk a gomb elementet
+buttonFormSim.textContent = 'hozzáadás';//a gomb belső szövegét feltöltjük
+formSim.appendChild(buttonFormSim)//a gombot hozzáadjuk a fromhoz
+
 containerDiv.appendChild(tableDiv);//a tablaDiv-et hozzáadjuk a containerDivhez
 containerDiv.appendChild(formDiv);//a formDiv-et hozzáadjuk a containerDivhez
