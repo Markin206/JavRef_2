91ffe29aea21a431846657a68e089e9a4ed8ddf4
Markin206 <marci061123@gmail.com>
2025-04-24 16:24:40 +0200

    form letrehozasa oop

--- Alkotasok/oop/area.js
+++ Alkotasok/oop/area.js
@@ -55,3 +55,45 @@ class Table extends Area {
         table.appendChild(tbody);//a táblázathoz hozzáadjuk a tőrzsét
     }
 }
+
+/**
+ * A Form osztály az Area osztály leszármazottja
+ * az osztály felelős hogy létrehozza a formot, és annak fieldjeit,
+ * gombjait.
+ */
+class Form extends Area {
+    constructor(cssClass){//A form konstruktora ahol létrehozzuk a formot és annak fieldjeit
+        super(cssClass);//meghívjuk az Area konstruktorát hogy létrehozzuk a Form div elementét
+        const form = document.createElement('form');//létrehozzunk egy formot
+        this.div.appendChild(form);//az újonnan létrehozott divhez hozzáadjuk a formot
+        const fieldElementList = [{//létrehozunk egy tömböt amely tartalmaz 2 tulajdonságot fieldid amely tartalmazza a field Id-jét és fieldLabel amely tartalmazza a szöveget
+            fieldid: 'name',//a fieldid tulajdonság megkapja a name string értéket
+            fieldLabel: 'Szerző'//a fieldLabel megkapja a Szerző string értéket
+        },
+        {
+            fieldid: 'mufaj',//a fieldid tulajdonság megkapja a mufaj string értéket
+            fieldLabel: 'Műfaj'// a field label tulajdonság megkapja a Műfaj string értéket
+        },
+        {
+            fieldid: 'cim',//a fieldid tulajdonság megkapja a cim string értéket
+            fieldLabel: 'cím'// a field label tulajdonság megkapja a cím string értéket
+        }]
+        
+        for(const fieldElement of fieldElementList){//végig iterálunk a tömb egyes objektumain
+            const field = makeDiv('field');//létrehozzunk egy field divet
+            form.appendChild(field);//hozzáadjuk a field osztályú divet a formhoz
+            const label = document.createElement('label');//létrehozzunk egy label elementet
+            label.htmlFor = fieldElement.fieldid;//a label htmlFor tulajdonsága megkapja az objektum fieldid tulajdonságát
+            label.textContent = fieldElement.fieldLabel;//a label textContent tulajdonsága megkapja az objektum fieldLabel tulajdonságát
+            field.appendChild(label)//a labelt hozzáadjuk a fieldhez
+            const input = document.createElement('input');//létrehozzuk az input elementet
+            input.id = fieldElement.fieldid;//az input element id tulajdonsága megkapja az objektum fieldid tulajdonságát
+            field.appendChild(document.createElement('br'))//hozzáadunk a fieldhez egy létrehozott sortörést
+            field.appendChild(input)//a fieldhez hozzáadjuk az inputot
+        }
+        
+        const button = document.createElement('button');//létrehozunk egy gomb elementet
+        button.textContent = 'hozzáadás';//a tartalmát feltöltjük a hozzáadás string értékkel
+        form.appendChild(button)//a gombot hozzáadjuk a formhoz
+    }
+}
